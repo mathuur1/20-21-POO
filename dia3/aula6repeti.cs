77fee04776cbe5cacdd749bad95d99aeaead5fd5
mathuur1 <77595392+mathuur1@users.noreply.github.com>
2021-11-13 02:30:56 +0000

    aula-6-repetir-com-while

---
+++ dia3/aula6repeti.cs
@@ -0,0 +1,13 @@
+using System;
+class MainClass {
+  public static void Main (string[] args) {
+    int x = 1;
+    while (x <= 10) {
+       Console.WriteLine(x);
+       x++;
+      }
+  }
+}
+
+
+
